a2fb1b6f1c538af874576936a6a4f0f5a0e9dcc5
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2017-03-29 00:05:44 +0200

    Added init.firebase.example.js file

---
+++ www/js/init.firebase.example.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "<YOUR API KEY HERE>",
+  authDomain: "<YOUR AUTHDOMAIN HERE>",
+  databaseURL: "<YOUR DATABASE URL HERE>",
+  storageBucket: "<YOUR STORAGE BUCKET HERE>",
+  messagingSenderId: "<YOUR MESSAGING SENDER ID HERE>"
+};
+firebase.initializeApp(config);
